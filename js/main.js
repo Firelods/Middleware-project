d85826ec9ebb4ca46383bd153b1420327a490ae3
Firelods <clement.lefevre1@etu.unice.fr>
2024-10-16 17:03:04 +0200

    Ajout de l'input départ fonctionnel

--- js/main.js
+++ js/main.js
@@ -1,30 +1,31 @@
-var map = L.map("map").setView([48.8566, 2.3522], 13); // Coordonnées de Paris par défaut
+var map = L.map("map").setView([48.8566, 2.3522], 13);
 
-// Ajouter les tuiles OSM
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
     attribution:
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
 
-// Ajouter un marqueur
-var marker = L.marker([48.8566, 2.3522])
-    .addTo(map)
-    .bindPopup("Un marqueur à Paris.")
-    .openPopup();
-    
-function showCustomNotification(title, message, type) {
-  // Récupération du composant de notification par son ID
-  const notificationComponent = document.getElementById("notification");
+window.addEventListener("centerMap", (event) => {
+    const { lat, lon } = event.detail;
 
-  // Appel de la méthode pour afficher la notification avec le titre et le message personnalisé
-  notificationComponent.showNotification(title, message, type);
+    map.setView([lat, lon], 13); // Recentrer la carte sur la position de l'utilisateur
+
+    L.marker([lat, lon]).addTo(map).bindPopup("Vous êtes ici").openPopup();
+});
+
+function showCustomNotification(title, message, type) {
+    // Récupération du composant de notification par son ID
+    const notificationComponent = document.getElementById("notification");
+
+    // Appel de la méthode pour afficher la notification avec le titre et le message personnalisé
+    notificationComponent.showNotification(title, message, type);
 }
 
 // Exemple d'appel pour tester
 document.addEventListener("DOMContentLoaded", () => {
-  showCustomNotification(
-    "Bienvenue !",
-    "Votre session a commencé avec succès.",
-    "error"
-  );
+    showCustomNotification(
+        "Bienvenue !",
+        "Votre session a commencé avec succès.",
+        "error"
+    );
 });
