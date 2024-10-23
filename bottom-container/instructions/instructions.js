b9eb1b6ea63f9a453fdc73c112808c0dd5f974a5
Antoine-FdRg <antoine.fadda-rodriguez@etu.unice.fr>
2024-10-23 13:02:54 +0200

    ✨ ajout de méthodes pour mettre à jour le composant instruction #4

--- bottom-container/instructions/instructions.js
+++ bottom-container/instructions/instructions.js
@@ -35,6 +35,39 @@ class InstructionsComponent extends HTMLElement {
       window.dispatchEvent(new CustomEvent("displayInputLocationComponent"));
     });
   }
+
+  updateArrivalTime(hour, minutes) {
+    if (!hour || !minutes) {
+      console.error("Heure ou minutes manquantes");
+      return;
+    }
+    if (hour < 0 || hour > 23) {
+      console.error("Heure invalide");
+      return;
+    }
+    if (minutes < 0 || minutes > 59) {
+      console.error("Minutes invalides");
+      return;
+    }
+    if (hour < 10) {
+      hour = "0" + hour;
+    }
+    if (minutes < 10) {
+      minutes = "0" + minutes;
+    }
+    const arrivalTime = document.getElementById("arrival");
+    arrivalTime.textContent = `${hour}h${minutes}`;
+  }
+
+  udpateDirectionIcon(direction) {
+    const directionIcon = document.getElementById("direction-icon");
+    directionIcon.src = `img/directions-icon/${direction}.svg`;
+  }
+
+  updateDirectionInstructions(instructions) {
+    const directionInstructions = document.getElementById("instruction");
+    directionInstructions.textContent = instructions;
+  }
 }
 
 customElements.define("instructions-component", InstructionsComponent);
